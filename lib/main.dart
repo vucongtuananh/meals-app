e68ff5ec358a8e92cf3a8195558e3797f1a9503e
TUANANHVU\tuana <tuananhmr2k3@gmail.com>
2023-09-20 22:36:53 +0700

    done create tab screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:meals_app/screens/categories_screen.dart';
+import 'package:meals_app/screens/tabs_screen.dart';
 
 final theme = ThemeData(
     useMaterial3: true,
@@ -22,7 +23,7 @@ class App extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: theme,
-      home: const CategoriesScreen(),
+      home: const TabsScreen(),
     );
   }
 }
